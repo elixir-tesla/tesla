2809a194fcbdec19750e3966cdc8966d1e7671e5
Tymon Tobolski <i@teamon.eu>
2015-03-16 22:19:10 +0100

    Very basic structure + first test

--- lib/tesla.ex
+++ lib/tesla.ex
@@ -1,2 +1,28 @@
 defmodule Tesla do
+  defmacro __using__(_what) do
+    quote do
+      def get(url),   do: request(:get,  url)
+      def post(url),  do: request(:post,  url)
+
+      defp request(method, url) do
+        %Tesla.Env{
+          method: method,
+          url:    url
+        }
+      end
+
+      import Tesla
+    end
+  end
+
+  defmacro adapter(ad) do
+    quote do
+      @adapter unquote(ad)
+    end
+  end
+end
+
+defmodule Tesla.Env do
+  defstruct url:      "",
+            method:   nil
 end
--- test/tesla_test.exs
+++ test/tesla_test.exs
@@ -1,7 +1,25 @@
+defmodule EchoAdapter do
+  def call(env) do
+    env
+  end
+end
+
+defmodule EchoClient do
+  use Tesla
+
+  adapter EchoAdapter
+end
+
 defmodule TeslaTest do
   use ExUnit.Case
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "get" do
+    env = EchoClient.get("/foo")
+    assert env.method == :get
+  end
+
+  test "post" do
+    env = EchoClient.post("/foo")
+    assert env.method == :post
   end
 end
