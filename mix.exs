973fc732ae48c145dcf08a13ee2ff7cdff990784
Tymon Tobolski <i@teamon.eu>
2015-04-06 16:16:01 +0200

    Preparing for hex

--- mix.exs
+++ mix.exs
@@ -3,7 +3,9 @@ defmodule Tesla.Mixfile do
 
   def project do
     [app: :tesla,
-     version: "0.0.1",
+     version: "0.1.0",
+     description: description,
+     package: package,
      elixir: "~> 1.0",
      deps: deps(Mix.env),
      test_coverage: [tool: ExCoveralls]]
@@ -16,15 +18,16 @@ defmodule Tesla.Mixfile do
     [applications: [:logger]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type `mix help deps` for more examples and options
+  defp description do
+    "HTTP client library, with support for middleware and multiple adapters."
+  end
+
+  defp package do
+    [contributors: ["Tymon Tobolski"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/monterail/tesla"}]
+  end
+
   defp deps(:dev) do
     [{:ibrowse, github: "cmullaparthi/ibrowse", tag: "v4.1.1"},
      {:exjsx, "~> 3.1.0"},
