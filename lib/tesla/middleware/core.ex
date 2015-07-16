99f5464bf8a16d60dd3e83d084c08d7226373c6e
Tymon Tobolski <i@teamon.eu>
2015-07-16 16:36:39 +0200

    New DecodeRels middleware

--- lib/tesla/middleware/core.ex
+++ lib/tesla/middleware/core.ex
@@ -16,3 +16,23 @@ defmodule Tesla.Middleware.Headers do
     run.(%{env | headers: headers})
   end
 end
+
+defmodule Tesla.Middleware.DecodeRels do
+  def call(env, run, []) do
+    env = run.(env)
+
+    if env.headers['Link'] do
+      rels = env.headers['Link']
+        |> to_string
+        |> String.split(",")
+        |> Enum.map(&String.strip/1)
+        |> Enum.map(fn e -> Regex.run(~r/\A<(.+)>; rel="(.+)"\z/, e, capture: :all_but_first) |> List.to_tuple end)
+        |> Enum.reduce(%{}, fn ({url, key}, a) -> Dict.put(a, key, url) end)
+
+      env |> Map.put(:rels, rels)
+    else
+      env
+    end
+  end
+end
+
