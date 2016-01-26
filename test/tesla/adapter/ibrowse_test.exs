8d7e2d56a034ab32c12b473ab8977fd43867cbfe
Tymon Tobolski <i@teamon.eu>
2016-01-26 15:15:27 +0100

    [fix] Get rid of unused variables warnings

--- test/tesla/adapter/ibrowse_test.exs
+++ test/tesla/adapter/ibrowse_test.exs
@@ -14,13 +14,13 @@ defmodule IbrowseTest do
 
 
   test "async requests" do
-    {:ok, id} = Client.get("http://httpbin.org/ip", respond_to: self)
+    {:ok, _id} = Client.get("http://httpbin.org/ip", respond_to: self)
 
     assert_receive {:tesla_response, _}, 2000
   end
 
   test "async requests parameters" do
-    {:ok, id} = Client.get("http://httpbin.org/ip", respond_to: self)
+    {:ok, _id} = Client.get("http://httpbin.org/ip", respond_to: self)
 
     receive do
       {:tesla_response, res} ->
