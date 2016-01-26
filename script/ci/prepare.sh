3645484dd36552411f4c8943d060f4d82e57e378
Tymon Tobolski <i@teamon.eu>
2016-01-26 15:54:42 +0100

    [fix] Elixir version typo

--- script/ci/prepare.sh
+++ script/ci/prepare.sh
@@ -3,7 +3,7 @@
 set -e
 
 export ERLANG_VERSION="18.2"
-export ELIXIR_VERSION="v1.2.2"
+export ELIXIR_VERSION="v1.2.1"
 
 # If you have a elixir_buildpack.config, do this instead:
 #export ERLANG_VERSION=$(cat elixir_buildpack.config | grep erlang_version | tr "=" " " | awk '{ print $2 }')
