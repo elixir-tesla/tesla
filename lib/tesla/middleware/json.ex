7ac2f29b5ec45dedb2b5bc7e984677d88a6988bc
Tymon Tobolski <i@teamon.eu>
2016-08-08 19:21:22 +0200

    Refactored DecodeJson middleware
    Moved checks into separate functions.

--- lib/tesla/middleware/json.ex
+++ lib/tesla/middleware/json.ex
@@ -1,10 +1,14 @@
 defmodule Tesla.Middleware.DecodeJson do
+  # NOTE: text/javascript added to support Facebook Graph API.
+  #       see https://github.com/teamon/tesla/pull/13
+  @valid_content_types ["application/json", "text/javascript"]
+
   def call(env, run, opts \\ []) do
     decode = opts[:decode] || &JSX.decode/1
 
     env = run.(env)
 
-    if is_json_content(env) do
+    if json?(env) do
       {:ok, body} = decode.(to_string(env.body))
 
       %{env | body: body}
@@ -13,13 +17,18 @@ defmodule Tesla.Middleware.DecodeJson do
     end
   end
 
-  def is_json_content(env) do
-    content_type = to_string(env.headers['Content-Type'])
-    valid_types = ["application/json", "text/javascript"]
-    is_valid_type = Enum.find(valid_types, fn(x) -> String.starts_with?(content_type, x) end)
-    is_valid_type && (is_binary(env.body) || is_list(env.body))
+  def json?(env) do
+    valid_content_type?(env) && parsable_body?(env)
   end
 
+  def valid_content_type?(env) do
+    content_type = to_string(env.headers['Content-Type'])
+    Enum.find(@valid_content_types, fn(x) -> String.starts_with?(content_type, x) end)
+  end
+
+  def parsable_body?(env) do
+    is_binary(env.body) || is_list(env.body)
+  end
 end
 
 defmodule Tesla.Middleware.EncodeJson do
