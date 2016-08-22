05354ee97faf6e401d5ee9e99e0f387388499b9a
Tymon Tobolski <i@teamon.eu>
2016-08-22 09:57:40 +0200

    basic browse adapter tests

--- test/tesla/adapter/ibrowse_test.exs
+++ test/tesla/adapter/ibrowse_test.exs
@@ -12,6 +12,16 @@ defmodule IbrowseTest do
     :ok
   end
 
+  test "basic get request" do
+    response = Client.get("http://httpbin.org/ip")
+    assert response.status == 200
+  end
+
+  test "basic post request" do
+    response = Client.post("http://httpbin.org/post", "some-post-data")
+    assert response.status == 200
+    assert Regex.match?(~r/some-post-data/, to_string(response.body))
+  end
 
   test "async requests" do
     {:ok, _id} = Client.get("http://httpbin.org/ip", respond_to: self)
