fcb152fce7bbe8d2ac3b218a47fe5691b9d45220
Tymon Tobolski <i@teamon.eu>
2016-08-26 14:24:54 +0200

    handle opts and error in hackney

--- lib/tesla/adapter/hackney.ex
+++ lib/tesla/adapter/hackney.ex
@@ -14,21 +14,22 @@ defmodule Tesla.Adapter.Hackney do
   end
 
   defp request(env) do
-    request(env.method, to_char_list(env.url), Enum.into(env.headers, []), env.body)
+    request(env.method, to_char_list(env.url), Enum.into(env.headers, []), env.body, env.opts)
   end
-  defp request(method, url, headers, %Stream{} = body), do: request_stream(method, url, headers, body)
-  defp request(method, url, headers, body) when is_function(body), do: request_stream(method, url, headers, body)
-  defp request(method, url, headers, body) do
-    handle :hackney.request(method, url, headers, body || '')
+  defp request(method, url, headers, %Stream{} = body, opts), do: request_stream(method, url, headers, body, opts)
+  defp request(method, url, headers, body, opts) when is_function(body), do: request_stream(method, url, headers, body, opts)
+  defp request(method, url, headers, body, opts) do
+    handle :hackney.request(method, url, headers, body || '', opts)
   end
 
 
-  defp request_stream(method, url, headers, body) do
-    {:ok, ref} = :hackney.request(method, url, headers, :stream)
+  defp request_stream(method, url, headers, body, opts) do
+    {:ok, ref} = :hackney.request(method, url, headers, :stream, opts)
     for data <- body, do: :ok = :hackney.send_body(ref, data)
     handle :hackney.start_response(ref)
   end
 
+  defp handle({:error, _} = error), do: error
   defp handle({:ok, status, headers}), do: {:ok, status, headers, []}
   defp handle({:ok, status, headers, ref}) do
     with {:ok, body} <- :hackney.body(ref) do
