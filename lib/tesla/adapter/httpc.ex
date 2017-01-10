2fccb798592a8c0210fd7e6bf55e7eed3d72ce98
Tobiasz <tobiasz.malecki@appunite.com>
2017-01-10 18:56:30 +0100

    Add support for Httpc options

--- lib/tesla/adapter/httpc.ex
+++ lib/tesla/adapter/httpc.ex
@@ -1,6 +1,8 @@
 defmodule Tesla.Adapter.Httpc do
-  def call(env, _opts) do
-    with {:ok, {status, headers, body}} <- request(env) do
+  @http_opts ~w(timeout connect_timeout ssl essl autoredirect proxy_auth version relaxed url_encode)a
+
+  def call(env, opts) do
+    with {:ok, {status, headers, body}} <- request(env, opts || []) do
       format_response(env, status, headers, body)
     end
   end
@@ -11,23 +13,24 @@ defmodule Tesla.Adapter.Httpc do
             body:     body}
   end
 
-  defp request(env) do
+  defp request(env, opts) do
     content_type = to_char_list(env.headers["content-type"] || "")
     handle request(
       env.method || :get,
       Tesla.build_url(env.url, env.query) |> to_char_list,
       Enum.into(env.headers, [], fn {k,v} -> {to_char_list(k), to_char_list(v)} end),
       content_type,
-      env.body
+      env.body,
+      Keyword.split(opts ++ env.opts, @http_opts)
     )
   end
 
-  defp request(method, url, headers, _content_type, nil) do
-    :httpc.request(method, {url, headers}, [], [])
+  defp request(method, url, headers, _content_type, nil, {http_opts, opts}) do
+    :httpc.request(method, {url, headers}, http_opts, opts)
   end
 
-  defp request(method, url, headers, content_type, body) do
-    :httpc.request(method, {url, headers, content_type, body}, [], [])
+  defp request(method, url, headers, content_type, body, {http_opts, opts}) do
+    :httpc.request(method, {url, headers, content_type, body}, http_opts, opts)
   end
 
   defp handle({:error, {:failed_connect, _}}), do: {:error, :econnrefused}
