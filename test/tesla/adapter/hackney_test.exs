e68a5b2798a207fe499986184fea48b6edc17b28
Tobiasz <tobiasz.malecki@appunite.com>
2017-09-11 10:54:54 +0200

    fix for hackney with_body option

--- test/tesla/adapter/hackney_test.exs
+++ test/tesla/adapter/hackney_test.exs
@@ -3,4 +3,15 @@ defmodule Tesla.Adapter.HackneyTest do
   use Tesla.Adapter.TestCase.Basic, adapter: :hackney
   use Tesla.Adapter.TestCase.StreamRequestBody, adapter: :hackney
   use Tesla.Adapter.TestCase.SSL, adapter: :hackney
+
+  test "get with `with_body: true` option" do
+    defmodule Client do
+      use Tesla
+
+      adapter :hackney, with_body: true
+    end
+
+    response = Client.get("#{Tesla.Adapter.TestCase.http_url()}/ip")
+    assert response.status == 200
+  end
 end
