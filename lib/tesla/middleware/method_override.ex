3a0071d82467d45d360189d956edb955ff0bae2e
Tymon Tobolski <i@teamon.eu>
2017-09-30 14:01:07 +0200

    MethodOverride docs

--- lib/tesla/middleware/method_override.ex
+++ lib/tesla/middleware/method_override.ex
@@ -1,13 +1,24 @@
 defmodule Tesla.Middleware.MethodOverride do
+  @behaviour Tesla.Middleware
+
   @moduledoc """
   Middleware that adds X-Http-Method-Override header with original request
   method and sends the request as post.
 
   Useful when there's an issue with sending non-post request.
 
-  Available options:
+  ### Example
+  ```
+  defmodule MyClient do
+    use Tesla
+
+    plug Tesla.Middleware.MethodOverride
+  end
+  ```
+
+  ### Options
   - `:override` - list of http methods that should be overriden,
-  everything except get and post if not specified
+  everything except `:get` and `:post` if not specified
   """
 
   def call(env, next, opts) do
@@ -21,13 +32,13 @@ defmodule Tesla.Middleware.MethodOverride do
     end
   end
 
-  def override(env) do
+  defp override(env) do
     env
     |> Tesla.Middleware.Headers.call([], %{"X-Http-Method-Override" => "#{env.method}"})
     |> Map.put(:method, :post)
   end
 
-  def overridable?(env, opts) do
+  defp overridable?(env, opts) do
     if opts[:override] do
       env.method in opts[:override]
     else
