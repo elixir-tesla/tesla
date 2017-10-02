acaf4cb806927887eaefa2a8340e1ed6ad14e4ab
Tymon Tobolski <i@teamon.eu>
2017-10-02 11:10:23 +0200

    Test DecodeRels without Link header

--- test/tesla/middleware/decode_rels_test.exs
+++ test/tesla/middleware/decode_rels_test.exs
@@ -1,27 +1,36 @@
 defmodule Tesla.Middleware.DecodeRelsTest do
   use ExUnit.Case
 
-  alias Tesla.Env
-
   defmodule Client do
     use Tesla
 
     plug Tesla.Middleware.DecodeRels
 
-    adapter fn _env ->
-      %Env{headers: %{
-        "Link" => ~s(<https://api.github.com/resource?page=2>; rel="next",
-        <https://api.github.com/resource?page=5>; rel="last")
-      }}
+    adapter fn env ->
+      case env.url do
+        "/rels" ->
+          %{env | headers: %{
+            "Link" => ~s(<https://api.github.com/resource?page=2>; rel="next",
+            <https://api.github.com/resource?page=5>; rel="last")
+          }}
+        _ ->
+          env
+      end
     end
   end
 
   test "deocde rels" do
-    env = Client.get("/")
+    env = Client.get("/rels")
 
     assert env.opts[:rels] == %{
       "next" => "https://api.github.com/resource?page=2",
       "last" => "https://api.github.com/resource?page=5"
     }
+  end
+
+  test "skip if no Link header" do
+    env = Client.get("/")
+
+    assert env.opts[:rels] == nil
   end
 end
