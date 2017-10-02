3f240ce0785b6137fcb8773abed0b8cc38c17b53
Tymon Tobolski <i@teamon.eu>
2017-10-02 10:12:01 +0200

    BaseUrl test

--- test/tesla/middleware/core_test.exs
+++ test/tesla/middleware/core_test.exs
@@ -33,37 +33,6 @@ defmodule CoreTest do
     end
   end
 
-  describe "Tesla.Middleware.BaseUrl" do
-    alias Tesla.Middleware.BaseUrl
-    use Tesla.MiddlewareCase, middleware: BaseUrl
-
-    test "base without slash, path without slash" do
-      env = BaseUrl.call(%Tesla.Env{url: "path"}, [], "http://example.com")
-      assert env.url == "http://example.com/path"
-    end
-
-    test "base without slash, path with slash" do
-      env = BaseUrl.call(%Tesla.Env{url: "/path"}, [], "http://example.com")
-      assert env.url == "http://example.com/path"
-    end
-
-    test "base with slash, path without slash" do
-      env = BaseUrl.call(%Tesla.Env{url: "path"}, [], "http://example.com/")
-      assert env.url == "http://example.com/path"
-    end
-
-    test "base with slash, path with slash" do
-      env = BaseUrl.call(%Tesla.Env{url: "/path"}, [], "http://example.com/")
-      assert env.url == "http://example.com/path"
-    end
-
-    test "skip double append" do
-      env = BaseUrl.call(%Tesla.Env{url: "http://other.foo"}, [], "http://example.com")
-      assert env.url == "http://other.foo"
-    end
-  end
-
-
 
   describe "Tesla.Middleware.Query" do
     alias Tesla.Middleware.Query
