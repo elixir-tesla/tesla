7808770e56d5292a0eae6c46304229d07589c8a6
Tymon Tobolski <i@teamon.eu>
2017-10-18 23:21:38 +0200

    Add .formatter.exs
    Code not formatted yet, but let's be prepared for
    1.6 release.

---
+++ .formatter.exs
@@ -0,0 +1,13 @@
+[
+  inputs: [
+    "lib/**/*.{ex,exs}",
+    "test/**/*.{ex,exs}",
+    "mix.exs"
+  ],
+  locals_without_parens: [
+    plug: 1,
+    plug: 2,
+    adapter: 1,
+    adapter: 2
+  ]
+]
