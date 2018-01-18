852d9f60b67ddc22d12be072366d7028568c6275
Giovanni Visciano <giovanni_visciano@yahoo.it>
2018-01-18 14:50:15 +0100

    export formatting

--- .formatter.exs
+++ .formatter.exs
@@ -1,13 +1,17 @@
+# Used by "mix format" and to export configuration.
+export_locals_without_parens = [
+  plug: 1,
+  plug: 2,
+  adapter: 1,
+  adapter: 2
+]
+
 [
   inputs: [
     "lib/**/*.{ex,exs}",
     "test/**/*.{ex,exs}",
     "mix.exs"
   ],
-  locals_without_parens: [
-    plug: 1,
-    plug: 2,
-    adapter: 1,
-    adapter: 2
-  ]
+  locals_without_parens: export_locals_without_parens,
+  export: [locals_without_parens: export_locals_without_parens]
 ]
