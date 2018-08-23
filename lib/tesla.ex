d98ef4b1434970a94393f8ec4c2e4ff024f23e1d
Tobiasz <tobiasz.malecki@appunite.com>
2018-08-23 21:15:20 +0200

    added Tesla.Env moduledoc

--- lib/tesla.ex
+++ lib/tesla.ex
@@ -7,6 +7,25 @@ defmodule Tesla.Error do
 end
 
 defmodule Tesla.Env do
+  @moduledoc """
+  This module defines a `t:Tesla.Env.t/0` struct that stores all data related to request/response.
+
+  ## Fields
+
+    * `:method` - method of request. Example: `:get`
+    * `:url` - request url. Example: `"https://www.google.com"`
+    * `:query` - list of query params.
+      Example: `[{"param", "value"}]` will be translated to `?params=value`.
+      Note: query params passed in url (e.g. `"/get?param=value"`) are not parsed to `query` field.
+    * `:headers` - list of request/response headers.
+      Example: `[{"content-type", "application/json"}].
+      Note: request headers are overriden by response headers when adapter is called.
+    * `:body` - request/response body.
+      Note: request body is overriden by response body when adapter is called.
+    * `:status` - response status. Example: `200`
+    * `:opts` - list of options. Example: `[adapter: [recv_timeout: 30_000]]`
+  """
+
   @type client :: Tesla.Client.t()
   @type method :: :head | :get | :delete | :trace | :options | :post | :put | :patch
   @type url :: binary
