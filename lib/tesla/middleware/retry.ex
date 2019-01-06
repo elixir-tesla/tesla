ee77698d043e86a88b8de41ccd265f01e4a8e25f
Ryan SIU <ryan.siu@tfidm.com>
2019-01-06 10:59:45 +0800

    Replace the retry checking with a function

--- lib/tesla/middleware/retry.ex
+++ lib/tesla/middleware/retry.ex
@@ -17,6 +17,7 @@ defmodule Tesla.Middleware.Retry do
   ### Options
   - `:delay`        - number of milliseconds to wait before retrying (defaults to 1000)
   - `:max_retries`  - maximum number of retries (defaults to 5)
+  - `:should_retry` - function to determine it should be retried
   """
 
   @defaults [
@@ -29,22 +30,21 @@ defmodule Tesla.Middleware.Retry do
     opts = opts || []
     delay = Keyword.get(opts, :delay, @defaults[:delay])
     max_retries = Keyword.get(opts, :max_retries, @defaults[:max_retries])
+    should_retry = Keyword.get(opts, :should_retry, &match?({:error, _}, &1))
 
-    retry(env, next, delay, max_retries)
+    retry(env, next, delay, max_retries, should_retry)
   end
 
-  defp retry(env, next, _delay, retries) when retries <= 1 do
+  defp retry(env, next, _delay, retries, _should_retry) when retries <= 1 do
     Tesla.run(env, next)
   end
 
-  defp retry(env, next, delay, retries) do
-    case Tesla.run(env, next) do
-      {:ok, env} ->
-        {:ok, env}
-
-      {:error, _reason} ->
-        :timer.sleep(delay)
-        retry(env, next, delay, retries - 1)
+  defp retry(env, next, delay, retries, should_retry) do
+    if should_retry.(Tesla.run(env, next)) do
+      :timer.sleep(delay)
+      retry(env, next, delay, retries - 1, should_retry)
+    else
+      {:ok, env}
     end
   end
 end
