7bb05bc5ffd2a6676f51da10e764a9a874a2c444
Ryan SIU <ryan.siu@tfidm.com>
2019-01-07 22:52:44 +0800

    Updated documentation for telemetry middleware

--- lib/tesla/middleware/telemetry.ex
+++ lib/tesla/middleware/telemetry.ex
@@ -2,6 +2,30 @@ defmodule Tesla.Middleware.Telemetry do
   @behaviour Tesla.Middleware
 
   @moduledoc """
+  Send the request duration and meta-information through telemetry.
+
+  ### Example usage
+  ```
+  defmodule TelemetryHandler do
+    use GenServer
+
+    def start_link(_, _) do
+      :telemetry_app.start(nil, nil)
+      :telemetry.attach(
+        "tesla-telemetry",
+        [:tesla, :telemetry, :traffic],
+        fn ([:tesla, :telemetry, :traffic], time, meta, _config) -> # Do sth end,
+        nil)
+    end
+  end
+
+  defmodule MyClient do
+    use Tesla
+
+    plug Tesla.Middleware.Telemetry
+
+  end
+  ```
   """
 
   @doc false
