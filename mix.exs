b2117e914d22d75d9646899cbd66b24f55356ed9
Ryan SIU <ryan.siu@tfidm.com>
2019-05-10 09:01:42 +0800

    Update the mint adapter docs

--- mix.exs
+++ mix.exs
@@ -61,8 +61,8 @@ defmodule Tesla.Mixfile do
       {:ibrowse, "~> 4.4.0", optional: true},
       {:hackney, "~> 1.6", optional: true},
       {:gun, "~> 1.3", optional: true},
-      {:castore, "~> 0.1.0"},
-      {:mint, "~> 0.2.0"},
+      {:castore, "~> 0.1.0", optional: true},
+      {:mint, "~> 0.2.0", optional: true},
 
       # json parsers
       {:jason, ">= 1.0.0", optional: true},
@@ -96,7 +96,8 @@ defmodule Tesla.Mixfile do
           Tesla.Adapter.Hackney,
           Tesla.Adapter.Httpc,
           Tesla.Adapter.Ibrowse,
-          Tesla.Adapter.Gun
+          Tesla.Adapter.Gun,
+          Tesla.Adapter.Mint
         ],
         Middlewares: [
           Tesla.Middleware.BaseUrl,
