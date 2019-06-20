fe6294b71a4d85522885d5ca08e52628e4d17f13
Alexander Strizhakov <alex.d3vils@gmail.com>
2019-06-20 06:44:39 +0300

    Update mix.exs
    Co-Authored-By: rinpatch <rinpatch@sdf.org>

--- mix.exs
+++ mix.exs
@@ -70,7 +70,7 @@ defmodule Tesla.Mixfile do
       # other
       {:fuse, "~> 2.4", optional: true},
       {:telemetry, "~> 0.3", optional: true},
-      {:cowlib, "~> 1.0.2", override: true},
+      {:cowlib, "~> 1.0.2", override: true, optional: true},
 
       # testing & docs
       {:excoveralls, "~> 0.8", only: :test},
