d2e5e808266000392e5ed252b72d7c8995a56fc9
Tymon Tobolski <i@teamon.eu>
2019-07-22 13:49:35 +0200

    Add more test cases

--- test/tesla/middleware/path_params_test.exs
+++ test/tesla/middleware/path_params_test.exs
@@ -14,4 +14,24 @@ defmodule Tesla.Middleware.PathParamsTest do
     assert {:ok, env} = @middleware.call(%Env{url: "/users/:id", opts: opts}, [], nil)
     assert env.url == "/users/42"
   end
+
+  test "value is not given" do
+    opts = [path_params: [y: 42]]
+    assert {:ok, env} = @middleware.call(%Env{url: "/users/:x", opts: opts}, [], nil)
+    assert env.url == "/users/:x"
+  end
+
+  test "value is nil" do
+    opts = [path_params: [id: nil]]
+    assert {:ok, env} = @middleware.call(%Env{url: "/users/:id", opts: opts}, [], nil)
+    assert env.url == "/users/:id"
+  end
+
+  test "placeholder contains another placeholder" do
+    opts = [path_params: [id: 1, id_post: 2]]
+
+    assert {:ok, env} = @middleware.call(%Env{url: "/users/:id/p/:id_post", opts: opts}, [], nil)
+
+    assert env.url == "/users/1/p/2"
+  end
 end
