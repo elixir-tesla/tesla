929b30c57a522445b94dcd79fc5a2d6c3774791c
Alex S <alex.strizhakov@gmail.com>
2019-08-11 12:20:31 +0300

    added some options for gun adapter with docs

--- test/tesla/adapter/gun_test.exs
+++ test/tesla/adapter/gun_test.exs
@@ -41,49 +41,91 @@ defmodule Tesla.Adapter.GunTest do
   test "response stream" do
     request = %Env{
       method: :get,
-      url: "#{@http}/stream/10"
+      url: "#{@http}/stream-bytes/10"
     }
 
     assert {:ok, %Env{} = response} = call(request)
     assert response.status == 200
+    assert byte_size(response.body) == 16
   end
 
   test "read response body in chunks" do
     request = %Env{
       method: :get,
-      url: "#{@http}/stream/10"
+      url: "#{@http}/stream-bytes/10"
     }
 
-    assert {:ok, %Env{} = response} = call(request, chunks_response: true)
+    assert {:ok, %Env{} = response} = call(request, body_as: :chunks)
     assert response.status == 200
     %{pid: pid, stream: stream, opts: opts} = response.body
-    assert opts[:chunks_response]
+    assert opts[:body_as] == :chunks
     assert is_pid(pid)
     assert is_reference(stream)
 
-    assert read_body(pid, stream) != []
+    assert read_body(pid, stream) |> byte_size() == 16
+    refute Process.alive?(pid)
   end
 
   test "read response body in stream" do
     request = %Env{
       method: :get,
-      url: "#{@http}/stream/10"
+      url: "#{@http}/stream-bytes/10"
     }
 
-    assert {:ok, %Env{} = response} = call(request, stream_response: true)
+    assert {:ok, %Env{} = response} = call(request, body_as: :stream)
     assert response.status == 200
     assert is_function(response.body)
-    assert Enum.to_list(response.body) != []
+    assert Enum.to_list(response.body) |> List.to_string() |> byte_size() == 16
   end
 
-  defp read_body(pid, stream, acc \\ []) do
+  test "read response body in stream with opened connection without closing connection" do
+    uri = URI.parse(@http)
+    {:ok, conn} = :gun.open(to_charlist(uri.host), uri.port)
+
+    request = %Env{
+      method: :get,
+      url: "#{@http}/stream-bytes/10"
+    }
+
+    assert {:ok, %Env{} = response} =
+             call(request, body_as: :stream, conn: conn, close_conn: false)
+
+    assert response.status == 200
+    assert is_function(response.body)
+    assert Enum.to_list(response.body) |> List.to_string() |> byte_size() == 16
+
+    assert Process.alive?(conn)
+
+    :ok = :gun.close(conn)
+    refute Process.alive?(conn)
+  end
+
+  test "read response body in stream with opened connection with closing connection" do
+    uri = URI.parse(@http)
+    {:ok, conn} = :gun.open(to_charlist(uri.host), uri.port)
+
+    request = %Env{
+      method: :get,
+      url: "#{@http}/stream-bytes/10"
+    }
+
+    assert {:ok, %Env{} = response} = call(request, body_as: :stream, conn: conn)
+
+    assert response.status == 200
+    assert is_function(response.body)
+    assert Enum.to_list(response.body) |> List.to_string() |> byte_size() == 16
+
+    refute Process.alive?(conn)
+  end
+
+  defp read_body(pid, stream, acc \\ "") do
     case Tesla.Adapter.Gun.read_chunk(pid, stream, timeout: 1_000) do
       {:fin, body} ->
         :ok = :gun.close(pid)
-        [body | acc]
+        acc <> body
 
       {:nofin, part} ->
-        read_body(pid, stream, [part | acc])
+        read_body(pid, stream, acc <> part)
     end
   end
 end
