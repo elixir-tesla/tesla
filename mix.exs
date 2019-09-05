4bd3242e953ef867fbbb3e7371ea0175974d8522
Tymon Tobolski <i@teamon.eu>
2019-09-05 18:41:06 +0200

    Update and relax mint dependencies
    Close #317

--- mix.exs
+++ mix.exs
@@ -61,8 +61,8 @@ defmodule Tesla.Mixfile do
       {:ibrowse, "~> 4.4.0", optional: true},
       {:hackney, "~> 1.6", optional: true},
       {:gun, "~> 1.3", optional: true},
-      {:castore, "~> 0.1.0", optional: true},
-      {:mint, "~> 0.2.0", optional: true},
+      {:castore, "~> 0.1", optional: true},
+      {:mint, "~> 0.4", optional: true},
 
       # json parsers
       {:jason, ">= 1.0.0", optional: true},
