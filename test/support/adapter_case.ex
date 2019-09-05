6eea7d79fa4d73b56399c9320b17bf773c9b9910
Tymon Tobolski <i@teamon.eu>
2019-09-05 13:43:47 +0200

    Fix ibrowse tests

--- test/support/adapter_case.ex
+++ test/support/adapter_case.ex
@@ -2,8 +2,8 @@ defmodule Tesla.AdapterCase do
   defmacro __using__(adapter: adapter) do
     quote do
       @adapter unquote(adapter)
-      @http "http://localhost:#{Application.get_env(:httparrot, :http_port)}"
-      @https "https://localhost:#{Application.get_env(:httparrot, :https_port)}"
+      @http "http://0.0.0.0:#{Application.get_env(:httparrot, :http_port)}"
+      @https "https://0.0.0.0:#{Application.get_env(:httparrot, :https_port)}"
 
       defp call(env, opts \\ []) do
         @adapter.call(env, opts)
