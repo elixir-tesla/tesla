cb348598c77a977cb589b026d65d7e7f8c9f0e0b
Alexander <alex.strizhakov@gmail.com>
2019-11-12 14:51:21 +0300

    fix for certification verification test

--- test/support/adapter_case.ex
+++ test/support/adapter_case.ex
@@ -2,8 +2,14 @@ defmodule Tesla.AdapterCase do
   defmacro __using__(adapter: adapter) do
     quote do
       @adapter unquote(adapter)
-      @http "http://0.0.0.0:#{Application.get_env(:httparrot, :http_port)}"
-      @https "https://0.0.0.0:#{Application.get_env(:httparrot, :https_port)}"
+      # Needed for SSL test with certificate verification
+      if @adapter == Tesla.Adapter.Ibrowse do
+        @http "http://0.0.0.0:#{Application.get_env(:httparrot, :http_port)}"
+        @https "https://0.0.0.0:#{Application.get_env(:httparrot, :https_port)}"
+      else
+        @http "http://localhost:#{Application.get_env(:httparrot, :http_port)}"
+        @https "https://localhost:#{Application.get_env(:httparrot, :https_port)}"
+      end
 
       defp call(env, opts \\ []) do
         @adapter.call(env, opts)
