8c27057940cff761f0961723baa30c42f2424906
Alexander <alex.strizhakov@gmail.com>
2019-11-13 13:05:05 +0300

    ibrowse fix

--- test/support/adapter_case.ex
+++ test/support/adapter_case.ex
@@ -2,14 +2,8 @@ defmodule Tesla.AdapterCase do
   defmacro __using__(adapter: adapter) do
     quote do
       @adapter unquote(adapter)
-      # Needed for SSL test with certificate verification
-      if @adapter == Tesla.Adapter.Ibrowse do
-        @http "http://0.0.0.0:#{Application.get_env(:httparrot, :http_port)}"
-        @https "https://0.0.0.0:#{Application.get_env(:httparrot, :https_port)}"
-      else
-        @http "http://localhost:#{Application.get_env(:httparrot, :http_port)}"
-        @https "https://localhost:#{Application.get_env(:httparrot, :https_port)}"
-      end
+      @http "http://localhost:#{Application.get_env(:httparrot, :http_port)}"
+      @https "https://localhost:#{Application.get_env(:httparrot, :https_port)}"
 
       defp call(env, opts \\ []) do
         @adapter.call(env, opts)
--- test/tesla/adapter/ibrowse_test.exs
+++ test/tesla/adapter/ibrowse_test.exs
@@ -2,6 +2,10 @@ defmodule Tesla.Adapter.IbrowseTest do
   use ExUnit.Case
 
   use Tesla.AdapterCase, adapter: Tesla.Adapter.Ibrowse
+
+  @http "http://0.0.0.0:#{Application.get_env(:httparrot, :http_port)}"
+  @https "https://0.0.0.0:#{Application.get_env(:httparrot, :https_port)}"
+
   use Tesla.AdapterCase.Basic
   use Tesla.AdapterCase.Multipart
   use Tesla.AdapterCase.StreamRequestBody
