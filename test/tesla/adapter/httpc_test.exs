063f26097c11c6d6d5551cfe999e7ed354ee4673
Yordis Prieto <yordis.prieto@gmail.com>
2023-10-10 18:19:43 -0400

    add badssl test

--- test/tesla/adapter/httpc_test.exs
+++ test/tesla/adapter/httpc_test.exs
@@ -45,4 +45,32 @@ defmodule Tesla.Adapter.HttpcTest do
 
     assert data["headers"]["content-type"] == "text/plain"
   end
+
+  describe "badssl" do
+    @describetag :integration
+
+    test "expired.badssl.com" do
+      assert {:error, :econnrefused} = Tesla.get(Tesla.client([], Tesla.Adapter.Httpc), "https://expired.badssl.com")
+    end
+
+    test "wrong.host.badssl.com" do
+      assert {:error, :econnrefused} = Tesla.get(Tesla.client([], Tesla.Adapter.Httpc), "https://wrong.host.badssl.com")
+    end
+
+    test "self-signed.badssl.com" do
+      assert {:error, :econnrefused} = Tesla.get(Tesla.client([], Tesla.Adapter.Httpc), "https://self-signed.badssl.com")
+    end
+
+    test "untrusted-root.badssl.com" do
+      assert {:error, :econnrefused} = Tesla.get(Tesla.client([], Tesla.Adapter.Httpc), "https://untrusted-root.badssl.com")
+    end
+
+    test "revoked.badssl.com" do
+      assert {:error, :econnrefused} = Tesla.get(Tesla.client([], Tesla.Adapter.Httpc), "https://revoked.badssl.com")
+    end
+#    TODO: figure out how to test this
+#    test "pinning-test.badssl.com" do
+#      assert {:error, :econnrefused} = Tesla.get(Tesla.client([], Tesla.Adapter.Httpc), "https://pinning-test.badssl.com")
+#    end
+  end
 end
