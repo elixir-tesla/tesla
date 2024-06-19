27cc13ee95f6dbd522b37ae6317dcfeefdb2c12f
Yordis Prieto <yordis.prieto@gmail.com>
2024-06-19 17:02:08 -0400

    fix: json decoder middleware for uppercase content type (#685)
    closes #680

--- test/tesla/middleware/json_test.exs
+++ test/tesla/middleware/json_test.exs
@@ -10,6 +10,10 @@ defmodule Tesla.Middleware.JsonTest do
       adapter fn env ->
         {status, headers, body} =
           case env.url do
+            # GET https://testlajson.free.beeceptor.com/todos to see the response
+            "/uppercased-content-type" ->
+              {200, [{"content-type", "application/JSON"}], "{\"value\": 123}"}
+
             "/decode" ->
               {200, [{"content-type", "application/json"}], "{\"value\": 123}"}
 
@@ -46,6 +50,11 @@ defmodule Tesla.Middleware.JsonTest do
 
         {:ok, %{env | status: status, headers: headers, body: body}}
       end
+    end
+
+    test "decoding json with insensitive content type" do
+      assert {:ok, env} = Client.get("/uppercased-content-type")
+      assert env.body == %{"value" => 123}
     end
 
     test "decode JSON body" do
