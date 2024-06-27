7e95271a90e62a88677354f0cd9ec7feff977340
Yordis Prieto <yordis.prieto@gmail.com>
2024-06-27 12:46:56 -0400

    chore: add explanation to keep request middleware (#688)

--- lib/tesla/middleware/keep_request.ex
+++ lib/tesla/middleware/keep_request.ex
@@ -1,6 +1,6 @@
 defmodule Tesla.Middleware.KeepRequest do
   @moduledoc """
-  Store request url ,body and headers into `:opts`.
+  Store request url, body and headers into `:opts`.
 
   ## Examples
 
@@ -26,6 +26,14 @@ defmodule Tesla.Middleware.KeepRequest do
   env.opts[:req_url]
   # => "http://localhost:8000/users/:user_id
   ```
+
+  ## Observability
+
+  In practice, you would combine `Tesla.Middleware.KeepRequest`, `Tesla.Middleware.PathParams`, and
+  `Tesla.Middleware.Telemetry` to observe the request and response data.
+  Keep in mind that the request order matters. Make sure to put `Tesla.Middleware.KeepRequest` before
+  `Tesla.Middleware.PathParams` to make sure that the request data is stored before the path parameters are replaced.
+  While keeping in mind that this is an application-specific concern, this is the overall recommendation.
   """
 
   @behaviour Tesla.Middleware
