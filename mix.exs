a9f4b66586a5918ef0e207e6fd4f452f7abf11a5
Jacob Parry <jacobparry@MacBook-Pro-4.localdomain>
2025-01-14 16:23:07 -0700

    bump-gun-version

--- mix.exs
+++ mix.exs
@@ -57,7 +57,7 @@ defmodule Tesla.Mixfile do
       # http clients
       {:ibrowse, "4.4.2", optional: true},
       {:hackney, "~> 1.6", optional: true},
-      {:gun, ">= 1.0.0", optional: true},
+      {:gun, ">= 2.0.0", optional: true},
       {:finch, "~> 0.13", optional: true},
       {:castore, "~> 0.1 or ~> 1.0", optional: true},
       {:mint, "~> 1.0", optional: true},
